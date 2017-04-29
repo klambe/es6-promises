ee2aa9ed5f199e6f3d1363737279aabed53a868c
klambe <klambe17@gmail.com>
2017-04-29 15:33:22 +0100

    added promise example

--- example-promise.js
+++ example-promise.js
@@ -1,13 +1,60 @@
-function getTempCallBack (location, callback){
-  callback(undefined,78);
-  callback('city not found');
+// function getTempCallBack (location, callback){
+//   callback(undefined,78);
+//   callback('city not found');
+// }
+//
+// getTempCallBack('dublin', function(err, temp){
+//   if(err){
+//     console.log('error: ',err);
+//   }else{
+//     console.log('success: ',temp);
+//   }
+// });
+//
+// function getTempPromise(location){
+//   return new Promise(function(resolve, reject){
+//     setTimeout(function(){
+//       resolve(79);
+//       reject('Not found from promise');
+//     },2000);
+//
+//   });
+// }
+//
+// getTempPromise('dublin').then(function(temp){
+//   console.log('Promise Success: ',temp);
+//
+// },function(err){
+//   console.log('Promise error: ',err);
+// })
 
-}
 
-getTempCallBack('dublin', function(err, temp){
-  if(err){
-    console.log('error: ',err);
-  }else{
-    console.log('success: ',temp);
+//exercies
+
+function addPromise(a, b){
+    return new Promise(function(resolve, reject){
+      setTimeout(function(){
+        if(typeof a === 'number' && typeof b === 'number'){
+        resolve(a+b);
+      }else{
+        reject('Numbers not valid');
+      }
+      },2000);
+
+    });
   }
-});
+
+
+  addPromise(5,2).then(function(result){
+    console.log('Promise Success result is: ',result);
+
+  },function(err){
+    console.log('Promise error numbers: ',err);
+  })
+
+  addPromise(5,"hello").then(function(result){
+    console.log('Promise Success result is: ',result);
+
+  },function(err){
+    console.log('Promise error numbers: ',err);
+  })
